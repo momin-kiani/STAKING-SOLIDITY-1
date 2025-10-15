7168e11636fe507685469454e4dbded4e940d92f
Momin Kiani <mominkiani73@gmail.com>
2025-10-15 04:25:08 -0700

    Updated staking logic and README

--- RewardToken.sol
+++ RewardToken.sol
@@ -6,6 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract RewardToken is ERC20{
     constructor(uint256 initialSupply) ERC20("RewardToken", "RWT") {
-        _mint(msg.sender, initialSupply);
+        _mint(msg.sender, initialSupply*10**18);
     }
 }
